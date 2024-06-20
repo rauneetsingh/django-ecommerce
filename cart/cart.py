8568fc762e3e7b859498284565e2e0dcb32d534e
Rauneet Singh <97212174+rauneetsingh@users.noreply.github.com>
2024-06-20 20:35:17 +0530

    initial commit

---
+++ cart/cart.py
@@ -0,0 +1,90 @@
+from store.models import Product
+
+class Cart():
+	def __init__(self, request):
+		self.session = request.session
+
+		# Get the current session key if it exists
+		cart = self.session.get('session_key')
+
+		# If the user is new, no session key! Create one!
+		if 'session_key' not in request.session:
+			cart = self.session['session_key'] = {}
+
+		# Make sure cart is available on all pages of site
+		self.cart = cart
+
+	def add(self, product, quantity):
+		product_id = str(product.id)
+		product_qty = str(quantity)
+
+		# Logic
+		if product_id in self.cart:
+			pass
+
+		else:
+			# self.cart[product_id] = {'price': str(product.price)}
+			self.cart[product_id] = int(product_qty)
+
+		self.session.modified = True
+
+	def __len__(self):
+		return len(self.cart)
+	
+	def get_prods(self):
+		# Get IDs from Cart
+		product_ids = self.cart.keys()
+
+		# Use IDs to lookup products in database model
+		products = Product.objects.filter(id__in=product_ids)
+
+		# Return those looked up Products
+		return products
+
+	def get_quants(self):
+		quantities = self.cart
+		return quantities
+
+	def update(self, product, quantity):
+		product_id = str(product)
+		product_qty = int(quantity)
+
+		# Get cart
+		ourcart = self.cart
+
+		# Update Dictionary/cart
+		ourcart[product_id] = product_qty
+
+		self.session.modified = True
+
+		thing = self.cart
+		return thing
+	
+	def delete(self, product):
+		product_id = str(product)
+
+		# Delete from dictionary/cart
+		if product_id in self.cart:
+			del self.cart[product_id]
+
+		self.session.modified = True
+
+	def cart_total(self):
+		# Get Product IDS
+		product_ids = self.cart.keys()
+
+		# Lookup those keys in our products database models
+		products = Product.objects.filter(id__in=product_ids)
+
+		# Get Quantities
+		quantities = self.cart
+
+		# Start counting at 0
+		total = 0
+		for key, value in quantities.items():
+			key = int(key)
+
+			for product in products:
+				if product.id == key:
+					total = total + (product.price * value)
+		return total
---
+++ store/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
+
+# Create your views here.
+
+def home(request):
+	products = Product.objects.all()
+	return render(request, 'home.html', {'products' : products})
+
+def login_user(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, 'You have been logged in')
+			return redirect('home')
+		else:
+			messages.success(request, 'There was an error, please try again...')
+			return redirect('login')
+
+	else:
+		return render(request, 'login.html', {})
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, 'You have been logged out... Thanks for stopping by...')
+	return redirect('home')
+
+def register_user(request):
+	form = SignUpForm()
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+
+			# Log in User
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, 'You have Registered Successfully!!')
+			return redirect('home')
+
+		else:
+			messages.success(request, 'Oops! There was a problem in registering you, please try again...')
+			return redirect('register')
+	else:
+		return render(request, 'register.html', {'form' : form})
+
+def product(request, pk):
+	product = Product.objects.get(id=pk)
+	return render(request, 'product.html', {'product' : product})	
